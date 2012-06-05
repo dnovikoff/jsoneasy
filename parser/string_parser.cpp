02284593092564f61c5ab5acecc6c9a73b602f63
Dmitri Novikov <novikov.dmitri.a@gmail.com>
2012-06-05 21:19:38 +0400

    cout removed

--- parser/string_parser.cpp
+++ parser/string_parser.cpp
@@ -10,16 +10,16 @@ string_parser::~string_parser() {}
 
 bool string_parser::operator()(const std::string& input, client::value_type& output ) {
 	namespace qi = boost::spirit::qi;
-	std:: cout << "Parsing: " << input << std::endl;
+//	std:: cout << "Parsing: " << input << std::endl;
 
 	iterator_type iter = input.begin();
 	iterator_type end = input.end();
 	bool r = qi::parse(iter, end, *grammar, output);
-	std:: cout << "Parse result: " << (r?"OK":"ERROR") << std::endl;
-	std:: cout << "Parsed to the end: " << ((iter == end)?"YES":"NO") << std::endl;
-	if(iter != end) {
-		std::cout << "Parse error near: " << std::string(iter, end) << std::endl;
-	}
+//	std:: cout << "Parse result: " << (r?"OK":"ERROR") << std::endl;
+//	std:: cout << "Parsed to the end: " << ((iter == end)?"YES":"NO") << std::endl;
+//	if(iter != end) {
+//		std::cout << "Parse error near: " << std::string(iter, end) << std::endl;
+//	}
 	return r && (iter == end);
 }
 
