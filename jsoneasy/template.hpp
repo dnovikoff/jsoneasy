93c30692de5cf1a5b75718e39bf39465ea8d283a
dnovikoff <novikov.dmitri.a@gmail.com>
2013-09-07 18:18:27 +0400

    support/all.hpp added

--- jsoneasy/template.hpp
+++ jsoneasy/template.hpp
@@ -6,23 +6,6 @@
  */
 
 #include <jsoneasy/template/create.hpp>
-
-// arrays
-#include <jsoneasy/template/support/list.hpp>
-#include <jsoneasy/template/support/vector.hpp>
-#include <jsoneasy/template/support/deque.hpp>
-#include <jsoneasy/template/support/set.hpp>
-#include <jsoneasy/template/support/mutliset.hpp>
-#include <jsoneasy/template/support/stack.hpp>
-#include <jsoneasy/template/support/pair.hpp>
-#include <jsoneasy/template/support/tuple.hpp>
-
-// objects
-#include <jsoneasy/template/support/map.hpp>
-#include <jsoneasy/template/support/multimap.hpp>
-
-// types
-#include <jsoneasy/template/support/optional.hpp>
-#include <jsoneasy/template/support/variant.hpp>
+#include <jsoneasy/template/support/all.hpp>
 
 #endif /* JSONEASY_TEMPLATE_HPP_ */
---
+++ jsoneasy/template/support/all.hpp
@@ -0,0 +1,27 @@
+#ifndef JSONEASY_TEMPLATE_ALL_HPP_
+#define JSONEASY_TEMPLATE_ALL_HPP_
+
+/**
+ * Support all default types with one include
+ */
+
+// arrays
+#include <jsoneasy/template/support/list.hpp>
+#include <jsoneasy/template/support/vector.hpp>
+#include <jsoneasy/template/support/deque.hpp>
+#include <jsoneasy/template/support/set.hpp>
+#include <jsoneasy/template/support/mutliset.hpp>
+#include <jsoneasy/template/support/stack.hpp>
+#include <jsoneasy/template/support/pair.hpp>
+#include <jsoneasy/template/support/tuple.hpp>
+
+// objects
+#include <jsoneasy/template/support/map.hpp>
+#include <jsoneasy/template/support/multimap.hpp>
+
+// types
+#include <jsoneasy/template/support/optional.hpp>
+#include <jsoneasy/template/support/variant.hpp>
+
+
+#endif /* JSONEASY_TEMPLATE_ALL_HPP_ */
