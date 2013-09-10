851abf774dae6fefbd111d628ba4dc39eef160c5
dnovikoff <novikov.dmitri.a@gmail.com>
2013-09-10 16:18:43 +0400

    Removed unused Trace class

--- test/compile/complex2.cpp
+++ test/compile/complex2.cpp
@@ -5,9 +5,6 @@
 #include <jsoneasy/template/support/optional.hpp>
 #include <jsoneasy/template/create.hpp>
 
-template<bool...>
-struct Trace {};
-
 void compile() {
 	typedef std::tuple<std::string, boost::optional<double> > t1;
 	t1 x;
