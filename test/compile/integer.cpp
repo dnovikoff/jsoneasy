f1928c5560dbfbce7675c77f07754ee44a8f27ad
dnovikoff <novikov.dmitri.a@gmail.com>
2013-09-10 19:13:13 +0400

    Integer compile test

--- test/compile/integer.cpp
+++ test/compile/integer.cpp
@@ -2,6 +2,6 @@
 #include <jsoneasy/template/create.hpp>
 
 void compile() {
-	std::tuple<int> mp;
+	std::tuple<size_t, uint8_t, int8_t, float, double, int64_t, uint64_t, int32_t, uint32_t, int, char, unsigned char> mp;
 	JsonEasy::Template::createHandler( mp );
 }
